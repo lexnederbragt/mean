1856527fac5ccb79bb54240000785d15fa512bda
Lex Nederbragt <lex.nederbragt@ibv.uio.no>
2018-02-09 14:48:22 +0100

    Fixed mean function for complex numbers

--- mean.py
+++ mean.py
@@ -1,6 +1,9 @@
 def mean(num_list):
     try:
-        return sum(num_list)/len(num_list)
+        mean = sum(num_list)/len(num_list)
+        if isinstance(mean , complex):
+            return NotImplemented
+        return mean
     except ZeroDivisionError as detail:
         msg = "\nCannot compute the mean value of an empty list."
         raise ZeroDivisionError(detail.__str__() +  msg)
