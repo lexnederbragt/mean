0efbe44bc89f53a2b4f6414c5c31f7d4b2a39a10
Lex Nederbragt <lex.nederbragt@ibv.uio.no>
2018-02-09 14:40:14 +0100

    Added test for complex numbers

--- test_mean.py
+++ test_mean.py
@@ -17,3 +17,11 @@ def test_long():
     obs = mean(range(1, big))
     exp = big/2.0
     assert obs == exp
+
+def test_complex():
+    # testing for complex numbers
+    # as arithmatic mean of complex numbers is meaningless
+    num_list = [2 + 3j, 3 + 4j, -32 - 2j]
+    obs = mean(num_list)
+    exp = NotImplemented
+    assert obs == exp
